d74a94db3725910f2442ca75c9fa74a9b33b191e
Joshua Fielding <Joshua.Fielding@fairfaxmedia.com.au>
2020-11-10 09:17:18 +1100

    updated dom

--- src/js/filter/index.js
+++ src/js/filter/index.js
@@ -12,6 +12,9 @@ const recipes = document.querySelectorAll(".recipe-single");
 // need to loop through all my recipes on the page, checking if their class name matches
 // the text content of the buttons
 
+// go through and make an array with all the matching data
+// e.g. deserts that are all, vegetarian, pescatarian
+
 mealBtns.forEach((meal) => {
   meal.addEventListener("click", (e) => {
     // remove/add the active class from the button
@@ -22,36 +25,64 @@ mealBtns.forEach((meal) => {
     let target = e.target.innerText;
 
     recipes.forEach((recipe) => {
-      // reset the filtering
-      recipe.classList.remove("active");
       if (target === "Entrees") {
         if (recipe.classList.contains("entree")) {
           recipe.classList.toggle("active");
+        } else {
+          recipe.classList.remove("active");
+          recipe.classList.add("inactive");
         }
       }
+
       if (target === "Mains") {
         if (recipe.classList.contains("main")) {
           recipe.classList.toggle("active");
+        } else {
+          recipe.classList.remove("active");
+          recipe.classList.add("inactive");
         }
       }
       if (target === "Salads") {
         if (recipe.classList.contains("salad")) {
           recipe.classList.toggle("active");
+        } else {
+          recipe.classList.remove("active");
+          recipe.classList.add("inactive");
         }
       }
+
+      // when I click on salad, I then need to check
+      // which classes match the diet types, then show them
+
+      // I initally want to show all salad types
+
+      // then if I click a salad that is only suitable for
+      // all, vegetarian and pescatarian, I need to hide
+      // the one/s that aren't suitable, in this case
+      // black bean salad
+
       if (target === "Sides") {
         if (recipe.classList.contains("side")) {
           recipe.classList.toggle("active");
+        } else {
+          recipe.classList.remove("active");
+          recipe.classList.add("inactive");
         }
       }
       if (target === "Desserts") {
         if (recipe.classList.contains("dessert")) {
           recipe.classList.toggle("active");
+        } else {
+          recipe.classList.remove("active");
+          recipe.classList.add("inactive");
         }
       }
       if (target === "Drinks") {
         if (recipe.classList.contains("drink")) {
           recipe.classList.toggle("active");
+        } else {
+          recipe.classList.remove("active");
+          recipe.classList.add("inactive");
         }
       }
     });
