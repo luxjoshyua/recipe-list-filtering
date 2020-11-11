954100c85d0b82bee25594d8a113d5657fade8ca
Joshua Fielding <Joshua.Fielding@fairfaxmedia.com.au>
2020-11-12 08:15:39 +1100

    added notes

--- src/js/filter/index.js
+++ src/js/filter/index.js
@@ -2,18 +2,10 @@
 
 // get the 6 meal types
 const mealBtns = document.querySelectorAll(".filter-types .filter-item");
-// console.log(mealTypes);
 // get the 5 dietary types
 const dietTypes = document.querySelectorAll(".filter-dietary .filter-item");
-// console.log(dietTypes);
 // select all the recipes on page
 const recipes = document.querySelectorAll(".recipe-single");
-
-// need to loop through all my recipes on the page, checking if their class name matches
-// the text content of the buttons
-
-// go through and make an array with all the matching data
-// e.g. deserts that are all, vegetarian, pescatarian
 
 mealBtns.forEach((meal) => {
   meal.addEventListener("click", (e) => {
@@ -22,6 +14,7 @@ mealBtns.forEach((meal) => {
       ? document.querySelector(".activeButton").classList.remove("activeButton")
       : "";
     meal.classList.add("activeButton");
+    // select the html target text
     let target = e.target.innerText;
 
     recipes.forEach((recipe) => {
@@ -42,6 +35,16 @@ mealBtns.forEach((meal) => {
           recipe.classList.add("inactive");
         }
       }
+
+      // when I click on salad, I need to show all that contain the salad class
+      // then I need to check the dietary types
+
+      // if i want the octopus salad,
+      // I need to check the salads that contain the classes pescatarian and all
+
+      // I then need to add an active class to those that do
+      // and add an inactive class to the others
+
       if (target === "Salads") {
         if (recipe.classList.contains("salad")) {
           recipe.classList.toggle("active");
@@ -50,16 +53,6 @@ mealBtns.forEach((meal) => {
           recipe.classList.add("inactive");
         }
       }
-
-      // when I click on salad, I then need to check
-      // which classes match the diet types, then show them
-
-      // I initally want to show all salad types
-
-      // then if I click a salad that is only suitable for
-      // all, vegetarian and pescatarian, I need to hide
-      // the one/s that aren't suitable, in this case
-      // black bean salad
 
       if (target === "Sides") {
         if (recipe.classList.contains("side")) {
