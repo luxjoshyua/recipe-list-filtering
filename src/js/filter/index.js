488837b187f912fac737ea5de75ff8e00e89d88f
Guillaume Pitot <gpitot@atlassian.com>
2020-11-12 09:32:49 +1100

    eg

--- src/js/filter/index.js
+++ src/js/filter/index.js
@@ -7,77 +7,65 @@ const dietTypes = document.querySelectorAll(".filter-dietary .filter-item");
 // select all the recipes on page
 const recipes = document.querySelectorAll(".recipe-single");
 
+let lastActiveMeal = null;
 mealBtns.forEach((meal) => {
   meal.addEventListener("click", (e) => {
     // remove/add the active class from the button
-    document.querySelector(".activeButton")
-      ? document.querySelector(".activeButton").classList.remove("activeButton")
-      : "";
-    meal.classList.add("activeButton");
-    // select the html target text
-    let target = e.target.innerText;
+    if (lastActiveMeal) {
+      lastActiveMeal.classList.remove("activeButton");
+    }
+    if (lastActiveMeal === meal) {
+      lastActiveMeal = null;
+    } else {
+      lastActiveMeal = meal;
+      meal.classList.add("activeButton");
+    }
 
-    recipes.forEach((recipe) => {
-      if (target === "Entrees") {
-        if (recipe.classList.contains("entree")) {
-          recipe.classList.toggle("active");
-        } else {
-          recipe.classList.remove("active");
-          recipe.classList.add("inactive");
-        }
-      }
-
-      if (target === "Mains") {
-        if (recipe.classList.contains("main")) {
-          recipe.classList.toggle("active");
-        } else {
-          recipe.classList.remove("active");
-          recipe.classList.add("inactive");
-        }
-      }
-
-      // when I click on salad, I need to show all that contain the salad class
-      // then I need to check the dietary types
-
-      // if i want the octopus salad,
-      // I need to check the salads that contain the classes pescatarian and all
-
-      // I then need to add an active class to those that do
-      // and add an inactive class to the others
-
-      if (target === "Salads") {
-        if (recipe.classList.contains("salad")) {
-          recipe.classList.toggle("active");
-        } else {
-          recipe.classList.remove("active");
-          recipe.classList.add("inactive");
-        }
-      }
-
-      if (target === "Sides") {
-        if (recipe.classList.contains("side")) {
-          recipe.classList.toggle("active");
-        } else {
-          recipe.classList.remove("active");
-          recipe.classList.add("inactive");
-        }
-      }
-      if (target === "Desserts") {
-        if (recipe.classList.contains("dessert")) {
-          recipe.classList.toggle("active");
-        } else {
-          recipe.classList.remove("active");
-          recipe.classList.add("inactive");
-        }
-      }
-      if (target === "Drinks") {
-        if (recipe.classList.contains("drink")) {
-          recipe.classList.toggle("active");
-        } else {
-          recipe.classList.remove("active");
-          recipe.classList.add("inactive");
-        }
-      }
-    });
+    filterReciplies();
   });
 });
+
+let lastActiveDiet = null;
+dietTypes.forEach((diet) => {
+  diet.addEventListener("click", (e) => {
+    // remove/add the active class from the button
+    if (lastActiveDiet) {
+      lastActiveDiet.classList.remove("activeButton");
+    }
+    if (lastActiveDiet === diet) {
+      lastActiveDiet = null;
+    } else {
+      lastActiveDiet = diet;
+      diet.classList.add("activeButton");
+    }
+
+    filterReciplies();
+  });
+});
+
+const filterReciplies = () => {
+  const activeMeal = lastActiveMeal
+    ? lastActiveMeal.getAttribute("data-meal-select")
+    : null;
+  const activeDiet = lastActiveDiet
+    ? lastActiveDiet.getAttribute("data-meal-filter")
+    : null;
+
+  recipes.forEach((recipe) => {
+    recipe.classList.remove("active");
+
+    // if nothing is selected show nothing
+    if (activeMeal === null && activeDiet === null) {
+      return;
+    }
+
+    const containsMeal =
+      activeMeal === null ? true : recipe.classList.contains(activeMeal);
+    const containsDiet =
+      activeDiet === null ? true : recipe.classList.contains(activeDiet);
+
+    if (containsMeal && containsDiet) {
+      recipe.classList.add("active");
+    }
+  });
+};
